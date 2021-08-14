bcfad451b0f03e8af35b76007fccd1bf4a20f039
marco <marcoavfcc@gmail.com>
2021-08-14 00:23:17 -0300

    Fusion Charts

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import ReactFC from 'react-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Chart from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-const Bar3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const Bar3d = ({ data }) => {
+  const chartConfigs = {
+    type: 'bar3d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      chart: {  
+        caption: 'Most Forked',
+        yAxisName: 'Forks',
+        xAxisName: 'Repos',
+        xAxisNameFontSize: '16px',
+        yAxisNameFontSize: '16px'
+      },
+      data
+    },
+  };
+  return <ReactFC {...chartConfigs} />
 };
 
-export default Bar3D;
+export default Bar3d;
--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import ReactFC from 'react-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Chart from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-const Column3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const column3d = ({ data }) => {
+  const chartConfigs = {
+    type: 'column3d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      chart: {  
+        caption: 'Most Popular',
+        yAxisName: 'Stars',
+        xAxisName: 'Repos',
+        xAxisNameFontSize: '16px',
+        yAxisNameFontSize: '16px'
+      },
+      data
+    },
+  };
+  return <ReactFC {...chartConfigs} />
 };
 
-export default Column3D;
+export default column3d;
